b14d5c6a42dc05371d53637860fce70613c432cb
jitendra kumar khasdev <jkhasdev@gmail.com>
2015-02-06 16:36:44 +0530

    start system programming. jk

--- persistent/global.c
+++ persistent/global.c
@@ -1,3 +1,14 @@
+/*
+ *
+ * File Name :  global.c
+ * Developer :  Jitendra kumar khasdev <jkhasdev@gmail.com>
+ *              @2015
+ * This program is developed only for the educational purposes,
+ * you are free to use and modify.
+ * Author    :  Jitendra kumar khasdev, Bhopal (INDIA)
+ *
+ */
+
 #include<stdio.h>
 
 static num=5;
@@ -5,6 +16,7 @@ int num;
 extern int num;
 
 int main(){
-printf("%d",num);
-return 0;
+
+	printf("%d",num);
+	return 0;
 }
--- persistent/opert.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-int x=1,y=2;
-if(--x && --y)
-printf("x=%d y=%d",x,y);
-else
-printf("%d %d",x,y);
-}
